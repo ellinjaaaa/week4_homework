a59817a360cb867741da1c5ade0ef2a472fb8cff
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-05 20:48:35 +0300

    feat: added a function for saving contacts with error handling

--- contacts.py
+++ contacts.py
@@ -6,7 +6,7 @@ contacts_file="contacts.json"
 
 def load_contacts():
     '''
-    Nolasa kontaktus no JSON faila, pārveidojot Python failā. Ja fails neeksistē, atgriež tukšu sarakstu.
+    Nolasa kontaktus no JSON faila, pārveidojot Python failā. Ja fails neeksistē vai bojāts, atgriež tukšu sarakstu.
     '''
     if not os.path.exists(contacts_file):
         return []
@@ -15,3 +15,15 @@ def load_contacts():
             return json.load(f)
     except (json.JSONDecodeError, OSError): #Ja fails bojāts kādā veidā, tiek atgriezts tukšs saraksts.
         return []
+    
+def save_contacts(contacts):
+    '''
+    Saglabā kontaktu sarakstu; Python -> JSON. Ja kļūda - paziņojums ar kļūdu.
+    '''
+    try:
+        with open(contacts_file, "w", encoding="utf-8") as f:
+            json.dump(contacts, f, indent=2, ensure_ascii=False)
+    except OSError as e:
+        print(f"Nevar saglabāt failu: {e}.") #Paglābj no programmas crashošanas vai citām kļūdām, 
+    #pie reizes norādot kļūdu (kas saglabāta, kā mainīgais e).
+                                        

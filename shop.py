a67b81188d021bf0aa185039a59e584e04b9ed79
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-09 23:30:51 +0300

    fix: changed and improved item adding logic: depending whether there is a price in database or not

--- shop.py
+++ shop.py
@@ -1,6 +1,6 @@
 import sys
 
-from storage import load_shopping, save_shopping, add_shopping, list_shopping, total_shopping, clear_shopping
+from storage import load_shopping, save_shopping, add_shopping, list_shopping, total_shopping, clear_shopping, load_prices, save_prices, get_price, set_price
 
 def shop():
     '''
@@ -16,11 +16,58 @@ def shop():
     cmd=sys.argv[1]
 
     if cmd=="add":
-        if len(sys.argv)<5:
-            print("Add komanda: add \"Produkts\" Daudzums Cena") #\, lai varētu izmantot tādas pašas pēdiņas iekšpusē.
+        if len(sys.argv)<4:
+            print("Add komanda: add \"Produkts\" Daudzums") #\, lai varētu izmantot tādas pašas pēdiņas iekšpusē.
             return
         
-        if add_shopping(products, sys.argv[2], sys.argv[3], sys.argv[4]): #If, jo funkcijā return True/False - saglabās, tikai ja True.
+        name=sys.argv[2]
+        qty=sys.argv[3]
+
+        price=get_price(name) #Cena meklēta datu bāzē.
+
+        #Ja cena nav atrasta
+        if price is None:
+            print("Cena nav zināma")
+
+            try:
+                price=float(input("Ievadi cenu: "))
+
+            except ValueError:
+                print("Nepareiza cena.")
+                return
+            
+            if price<=0:
+                print("Cenai jābūt pozitīvai.")
+                return
+            
+            set_price(name, price)
+
+            print(f"Cena saglabāta: {name} ({price:.2f} EUR)")
+
+        #Ja cena ir atrasta
+        else:
+            print(f"Atrasta cena: {price:.2f} EUR/gab.")
+            choice=input("[A]kceptēt / [M]ainīt?").lower()
+
+            if choice=="m":
+
+                try:
+                    price=float(input("Jaunā cena: "))
+
+                except ValueError:
+                    print("Nepareiza cena.")
+                    return
+                
+                if price<=0:
+                    print("Cenai jābūt pozitīvai.")
+                    return
+            
+                set_price(name, price)
+
+                print(f"Cena atjaunināta: {name} - {price:.2f} EUR")
+
+        #Pievieno produktu iepirkumu sarakstam
+        if add_shopping(products, name, qty, price):
             save_shopping(products)
 
     elif cmd=="list":
